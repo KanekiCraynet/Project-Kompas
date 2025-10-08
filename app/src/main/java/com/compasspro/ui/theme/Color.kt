1e6d1b858f15323a4183cc03cc4a803f2d180931
Dzakri Phalosa Nugroho <71388712+KanekiCraynet@users.noreply.github.com>
2025-10-09 00:20:06 +0700

    Create Color.kt

---
+++ app/src/main/java/com/compasspro/ui/theme/Color.kt
@@ -0,0 +1,57 @@
+package com.compasspro.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Colors
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val LightBackground = Color(0xFFF5F5F5)
+val LightSurface = Color(0xFFFFFFFF)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightOnSecondary = Color(0xFFFFFFFF)
+val LightOnTertiary = Color(0xFFFFFFFF)
+val LightOnBackground = Color(0xFF1C1B1F)
+val LightOnSurface = Color(0xFF1C1B1F)
+
+// Dark Theme Colors
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val DarkOnPrimary = Color(0xFF381E72)
+val DarkOnSecondary = Color(0xFF332D41)
+val DarkOnTertiary = Color(0xFF492532)
+val DarkOnBackground = Color(0xFFE6E1E5)
+val DarkOnSurface = Color(0xFFE6E1E5)
+
+// Compass Specific Colors
+val CompassRed = Color(0xFFE53E3E)
+val CompassBlue = Color(0xFF3182CE)
+val CompassGreen = Color(0xFF38A169)
+val CompassYellow = Color(0xFFD69E2E)
+val CompassOrange = Color(0xFFDD6B20)
+val CompassPurple = Color(0xFF805AD5)
+val CompassPink = Color(0xFFD53F8C)
+val CompassTeal = Color(0xFF319795)
+
+// Status Colors
+val SuccessColor = Color(0xFF10B981)
+val WarningColor = Color(0xFFF59E0B)
+val ErrorColor = Color(0xFFEF4444)
+val InfoColor = Color(0xFF3B82F6)
+
+// Gradient Colors
+val GradientStart = Color(0xFF667eea)
+val GradientEnd = Color(0xFF764ba2)
+val GradientAccent = Color(0xFFf093fb)
+
+// Wind Direction Colors
+val WindCalm = Color(0xFF6B7280)
+val WindLight = Color(0xFF10B981)
+val WindModerate = Color(0xFFF59E0B)
+val WindStrong = Color(0xFFEF4444)
+val WindVeryStrong = Color(0xFF7C2D12)
